e08d1ac96ceed7d3106dcb4fb26d3673e85ba30e
Brenno Rodrigues <brennopereira6@gmail.com>
2023-07-07 03:00:37 -0300

    feat(haskell): added parentheses to handle expressions precedence

--- haskell/app/Main.hs
+++ haskell/app/Main.hs
@@ -1,48 +1,62 @@
 module Main (main) where
 
-data SyntaxTree = Tree String SyntaxTree SyntaxTree | Leaf String | Any [Token] deriving (Show)
+data SyntaxTree = Operator String SyntaxTree SyntaxTree | Value String deriving (Show)
 
-data Token = Value String | Operator String deriving (Show, Eq)
+data Token = ValueToken String | OperatorToken String | LParenToken | RParenToken deriving (Show, Eq)
 
 operators :: [String]
-operators = ["+", "-", "*", "/"]
+operators = ["+", "-", "*", "/", "--"]
 
 evaluate :: SyntaxTree -> Int
-evaluate (Leaf value) = read value :: Int
-evaluate (Tree op left right)
+evaluate (Value value) = read value :: Int
+evaluate (Operator op left right)
     | op == "+" = (evaluate left) + (evaluate right)
     | op == "-" = (evaluate left) - (evaluate right)
     | op == "*" = (evaluate left) * (evaluate right)
     | op == "/" = (evaluate left) `div` (evaluate right)
-    | otherwise = evaluate $ Leaf "0"
-
-parseOperator :: [Token] -> SyntaxTree
-parseOperator [] = Leaf "Empty"
-parseOperator ((Value left):(Operator op):tokens) = Tree op (Leaf left) (parse tokens)
+    | op == "--" = evaluate left
+    | otherwise = evaluate $ Value "Invalid Operator"
 
 parseValue :: [Token] -> SyntaxTree
-parseValue [] = Leaf "Empty"
-parseValue [(Value value)] = Leaf value
-parseValue ((Value value):tokens) = parseOperator $ [Value value] ++ tokens
+parseValue ((ValueToken value):(OperatorToken op):tokens) = Operator op (parse tokens) (Value value)
+parseValue ((ValueToken value):_) = Value value
+parseValue _ = Value "Invalid Value"
+
+getScopeTokens :: [Token] -> Int -> [Token]
+getScopeTokens (token:tokens) currentLevel
+    | currentLevel == 0 = []
+    | token == LParenToken = token:(getScopeTokens tokens (currentLevel + 1))
+    | token == RParenToken = token:(getScopeTokens tokens (currentLevel - 1))
+    | otherwise = token:(getScopeTokens tokens currentLevel)
+getScopeTokens tokens currentLevel
+    | currentLevel == 0 = tokens
+    | otherwise = []
+
+parseOperator :: [Token] -> SyntaxTree -> SyntaxTree
+parseOperator ((OperatorToken op):tokens) carry = Operator op (parse tokens) carry
+parseOperator [RParenToken] carry = carry
+parseOperator _ carry = carry
 
 parse :: [Token] -> SyntaxTree
-parse [] = Leaf "Empty"
 parse tokens
-    | Value _ <- (head tokens) = parseValue tokens
-    | Operator _ <- (head tokens) = parseOperator tokens
+    | ValueToken _ <- (head tokens) = parseValue tokens
+    | LParenToken <- (head tokens) =
+        parseOperator
+            (drop (length $ getScopeTokens (tail tokens) 1) (tail tokens))
+            (parse $ getScopeTokens (tail tokens) 1)
+    | otherwise = Value "Parsing Error"
 
 tokenize :: [String] -> [Token]
 tokenize [] = []
-tokenize (arg:args) =
-    if (arg `elem` operators) then
-        [Operator arg] ++ (tokenize args)
-    else
-        [Value arg] ++ (tokenize args)
+tokenize (arg:args)
+    | arg == ")" = (tokenize args) ++ [LParenToken]
+    | arg == "(" = (tokenize args) ++ [RParenToken]
+    | arg `elem` operators = (tokenize args) ++ [OperatorToken arg]
+    | otherwise = (tokenize args) ++ [ValueToken arg]
 
 main :: IO ()
 main = do
-    fileContent <- readFile "input.mock"
+    fileContent <- readFile "input.example"
     let tokens = tokenize . words $ fileContent
-    print tokens
     print . parse $ tokens
     print . evaluate . parse $ tokens
